bc9e2fbf3886b00ef8dcd0223f437219075b9c7e
axtegenarnoldt <>
2024-02-12 14:38:43 +0000

    Add first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,31 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById ('question-box')
 
+const questionElement = document.getElementById ('question')
+const questionButtonsElement = document.getElementById ('answer-btn')
 startButton.addEventListener('click', startGame)
 
 function startGame() {
 console.log('started')
 startButton.classList.add('hide')
+questionContainerElement.classList.remove('hide')
 }
+
+function nextQuestion() {
+
+}
+
+/*
+ * Game Questions
+ */
+const question = [
+    {
+        question: 'Who is the lead vocalist of the band U2?',
+        answers: [
+            { text: 'Dave Grohl', correct: false },
+            { text: 'Bono', correct: ture },
+            { text: 'Paul McCartney', correct: false },
+            { text: 'Steve Perry', correct: false }
+        ]
+    }
+]
