9b543d000e9aa609dd618cf2f915c537b0a221a2
axtegenarnoldt <>
2024-02-13 10:48:26 +0000

    remove some content from script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,17 @@
-const startButton = document.getElementById('start-btn')
-const questionBoxElement = document.getElementById ('question-box')
+const startButton = document.getElementById('start-btn');
+const questionBoxElement = document.getElementById('question-box');
+const questionElement = document.getElementById('question');
 
-const questionElement = document.getElementById ('question')
-const answerButtonsElement = document.getElementById ('answer-btn')
-
-startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', startGame);
 
 function startGame() {
-console.log('started')
-startButton.classList.add('hide')
-questionBoxElement.classList.remove('hide')
-}
-
-function nextQuestion() {
-
+    console.log('started');
+    startButton.classList.add('hide');
+    questionBoxElement.classList.remove('hide');
 }
 
 function displayQuestion(question) {
-    questionElement.innerText = question.question
-    question.answer.forEach(answer => {
-        const button = document.createElement ('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
+    questionElement.innerText = question.question;
 }
 
 /*
