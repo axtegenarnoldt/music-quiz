cf3db29a4ca761334350329dba65996f91114c0e
Josefine axtegen arnoldt <axtegenarnoldt@gmail.com>
2024-02-15 09:45:46 +0000

    Add function to show answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,46 @@
 const startButton = document.getElementById('start-btn')
-const questionBoxElement = document.getElementById('question-box')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-box')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-btn')
 
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
 function startGame() {
-    console.log('started');
-    startButton.classList.add('hide');
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionBoxElement.classList.remove('hide');
-    setNextQuestion()
-}
-function setNextQuestion () {
-displayQuestion(shuffledQuestions[currentQuestionIndex])
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
-function displayQuestion(question) {
-    questionElement.innerText = question.question;
+function setNextQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function selectAnswer() {
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
 
+
+function selectAnswer(e) {
+  
 }
 
 /*
